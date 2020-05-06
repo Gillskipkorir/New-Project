95915b977d6406b48cb6c43fda0b0344a94b432d
Gills <30428840+Gillskipkorir@users.noreply.github.com>
2020-05-06 13:08:00 +0300

    Map added

--- app/src/main/java/com/kip/gillz/newproject/Splash_Screen.java
+++ app/src/main/java/com/kip/gillz/newproject/Splash_Screen.java
@@ -1,5 +1,7 @@
 package com.kip.gillz.newproject;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
+
+        // 3 seconds
+        int SPLASH_TIME_OUT = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(Splash_Screen.this,Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
